185b20a864f075e2584ab66a19c0ce9133e0eff7
Igor Antonov <antonov.igor.khv@gmail.com>
2022-12-24 13:38:41 +0300

    6.9. Подготовит конфигурацию настроек для подключения к MongoDB.
    В директории `config` создадим новый модуль `mongodb.config.ts` и опишем в неё функцию для получения настроек подключения к MongoDB. Функцию назовём `getMongoDbConfig`. Дополнительно определим пространство имён для группировки настроек подключения к базе данных (`database`). Для этого воспользуемся функцией `registerAs` из `@nestjs/config`. Затем подключим `MongooseModule` и добавим загрузку настроек. Валидацию опишем в `env.validation.ts`.

--- typoteka/apps/notify/src/app/app.constant.ts
+++ typoteka/apps/notify/src/app/app.constant.ts
@@ -1,1 +1,10 @@
 export const NOTIFY_SERVICE_ENV_PATH = 'environments/.notify.env';
+
+export enum EnvValidationMessage {
+  DBHostRequired = 'MongoDB host is required',
+  DBNameRequired = 'Database name is required',
+  DBPortRequired = 'MongoDB port is required',
+  DBUserRequired = 'MongoDB user is required',
+  DBPasswordRequired = 'MongoDB password is required',
+  DBBaseAuthRequired = 'MongoDB authentication base is required',
+}
