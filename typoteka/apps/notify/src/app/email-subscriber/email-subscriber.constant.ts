72f69253d89b06a41fe09766e2e72ce0b19df692
Igor Antonov <antonov.igor.khv@gmail.com>
2022-12-24 14:59:37 +0300

    6.13. Добавит валидацию для `CreateSubscriberDto`.
    Расставим декораторы из пакета `class-validator` для `CreateSubscriberDto`. Текст сообщений об ошибках зафиксируем в отдельных константах. В файле `main.ts` глобально подключим `ValidationPipe`.

---
+++ typoteka/apps/notify/src/app/email-subscriber/email-subscriber.constant.ts
@@ -0,0 +1,4 @@
+export const EMAIL_NOT_VALID = 'The email is not valid';
+export const LAST_NAME_IS_EMPTY = 'The last name is empty';
+export const FIRST_NAME_IS_EMPTY = 'The first name is empty';
+export const USER_ID_IS_EMPTY = 'The userId is empty';
