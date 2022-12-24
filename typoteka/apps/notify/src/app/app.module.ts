fb8cef3a954ebde31da1903a463fe577f72b903d
Igor Antonov <antonov.igor.khv@gmail.com>
2022-12-24 13:27:53 +0300

    6.8. Подключит модуль `ConfigModule` в Notify.
    Сервису Notify потребуется входные данные: настройки для подключения к MongoDB, RabbitMQ. Заведём констант `NOTIFY_SERVICE_ENV_PATH` и опишем в ней путь к `.notify.env`. Затем подключим `ConfigModule` в корневом модуле приложения.

--- typoteka/apps/notify/src/app/app.module.ts
+++ typoteka/apps/notify/src/app/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { NOTIFY_SERVICE_ENV_PATH } from './app.constant';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+      envFilePath: NOTIFY_SERVICE_ENV_PATH
+    }),
+  ],
   controllers: [],
   providers: [],
 })
