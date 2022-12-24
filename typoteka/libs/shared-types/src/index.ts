caffdbb371a3f2710ad8e54600d72b43e7bc4f04
Igor Antonov <antonov.igor.khv@gmail.com>
2022-12-24 13:04:02 +0300

    6.7. Добавит общий интерфейс `Subscriber`.
    Нам потребуется интерфейс для описания подписчиков. Объявим его в библиотеки `shared-types`. В интерфейс предусмотрим поля, которые потребуются для решения задачи отправки уведомлений.

--- typoteka/libs/shared-types/src/index.ts
+++ typoteka/libs/shared-types/src/index.ts
@@ -3,3 +3,4 @@ export * from './lib/user.interface';
 export * from './lib/post.interface';
 export * from './lib/comment.interface';
 export * from './lib/category.interface';
+export * from './lib/subscriber.interface';
