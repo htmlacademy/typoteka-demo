31743d35aae8a7ce4793c1ef65599343fbe1cd1c
Igor Antonov <antonov.igor.khv@gmail.com>
2022-12-27 14:11:56 +0300

    6.18. Добавит перечисление `CommandEvent`.
    В перечислении `CommandEvent` определим типы команд для операций на основе сообщений. Пока опредлим две из них: `AddSubscriber` (добавление подписчика) и `AddPost` (добавление публикации).

--- typoteka/libs/shared-types/src/index.ts
+++ typoteka/libs/shared-types/src/index.ts
@@ -4,3 +4,4 @@ export * from './lib/post.interface';
 export * from './lib/comment.interface';
 export * from './lib/category.interface';
 export * from './lib/subscriber.interface';
+export * from './lib/command-event.enum';
