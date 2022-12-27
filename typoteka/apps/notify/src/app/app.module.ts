bb198326332f1df584965de6ae1c3c0ff0df9de3
Igor Antonov <antonov.igor.khv@gmail.com>
2022-12-27 21:48:32 +0300

    6.20. Добавит модуль `EmailSubscriberModule`.
    Завершим начальную разработку сервиса Notify. Создадим модуль `EmailSubscriberModule` и подключим в него контроллер, Mongoose, и сервисы. Последним шагом подключим `EmailSubscriberModule` в `AppModule`.

--- typoteka/apps/notify/src/app/app.module.ts
+++ typoteka/apps/notify/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { getMongoDbConfig, mongoDbOptions } from '../../config/mongodb.config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { validateEnvironments } from './env.validation';
 import { rabbitMqOptions } from '../../config/rabbitmq.config';
+import { EmailSubscriberModule } from './email-subscriber/email-subscriber.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { rabbitMqOptions } from '../../config/rabbitmq.config';
       validate: validateEnvironments,
     }),
     MongooseModule.forRootAsync(getMongoDbConfig()),
+    EmailSubscriberModule,
   ],
   controllers: [],
   providers: [],
