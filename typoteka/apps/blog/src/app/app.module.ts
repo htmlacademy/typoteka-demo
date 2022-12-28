c50cc593e950281332f12ce2fef4e4a1cda2b005
Igor Antonov <antonov.igor.khv@gmail.com>
2022-12-28 12:55:03 +0300

    7.1. Добавит `ConfigModule` для сервиса `Blog`.
    Добавим поддержку работу с `ConfigModule` для сервиса `Blog`. Нам потребуется модуль для работы с конфигурацией, например, для того чтобы изменить порт на котором запускается приложение.

--- typoteka/apps/blog/src/app/app.module.ts
+++ typoteka/apps/blog/src/app/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { BlogPostModule } from './blog-post/blog-post.module';
 import { BlogCategoryModule } from './blog-category/blog-category.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import { ENV_FILE_PATH } from './app.constant';
 
 @Module({
-  imports: [PrismaModule, BlogPostModule, BlogCategoryModule],
+  imports: [
+    PrismaModule,
+    BlogPostModule,
+    BlogCategoryModule,
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+      envFilePath: ENV_FILE_PATH
+    })
+  ],
   controllers: [],
   providers: [],
 })
