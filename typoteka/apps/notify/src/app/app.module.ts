f99482a28c9bfb7519ccf8e84fbb5375c9b7cb10
Igor Antonov <antonov.igor.khv@gmail.com>
2022-12-28 11:49:37 +0300

    6.31. Добавит загрузку настроек для подключения к почтовому серверу.
    Почти всё готово для непосредственной отправки писем. Остаётся добавить загрузку настроек для установки соединения с почтовым сервером и подключить модуль `Mail` в `EmailSubscriber`. Начнём с решения первой задачи.
    
    В `app.module.ts` добавим загрузку настроек `mailOptions`.

--- typoteka/apps/notify/src/app/app.module.ts
+++ typoteka/apps/notify/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { validateEnvironments } from './env.validation';
 import { rabbitMqOptions } from '../../config/rabbitmq.config';
 import { EmailSubscriberModule } from './email-subscriber/email-subscriber.module';
+import { mailOptions } from '../../config/mail.config';
 
 @Module({
   imports: [
@@ -13,7 +14,7 @@ import { EmailSubscriberModule } from './email-subscriber/email-subscriber.modul
       cache: true,
       isGlobal: true,
       envFilePath: NOTIFY_SERVICE_ENV_PATH,
-      load: [mongoDbOptions, rabbitMqOptions],
+      load: [mongoDbOptions, rabbitMqOptions, mailOptions],
       validate: validateEnvironments,
     }),
     MongooseModule.forRootAsync(getMongoDbConfig()),
