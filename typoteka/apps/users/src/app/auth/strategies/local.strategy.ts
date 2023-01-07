756c18c0f7526c11e62fb46a2b1aec34a01d0dce
Igor Antonov <antonov.igor.khv@gmail.com>
2023-01-07 14:51:25 +0300

    8.2. Добавит LocalStrategy для Passport.
    Перед тем как приступить как добавить поддержку Refresh Token в сервис `Users` потренируемся работать со стратегиями аутентификации Passport.
    
    Проверка логина и пароля происходит прямо в обработчике маршрута `/auth/login`. Мы используем Passport, поэтому логично её реализовать в виде отдельной стратегии — `LocalStrategy`. Ранее мы создавали стратегию для проверки access token (далее AT), теперь реализуем похожую, но только для проверки логина и пароля.
    
    Создадим отдельный модуль `local.strategy.ts` и в нём опишем новый класс `LocalStrategy`. Не забудьте отнаследоваться от `PassportStrategy`.
    
    Этому классу потребуется доступ к сервису аутентификации (`AuthService`), поэтому сразу внедрим его в качестве зависимости. Внутри конструктора не забудем вызвать родительский конструктор. Аргументом ему нужно передать объект настроек. На текущем этапе из настроек нас интересует только переопределение `usernameField`.
    
    В этом поле необходимо передать информацию о названии поля из которого извлекается логин. В нашем случае это `email`.
    
    Последним шагом сделаем реализацию метода `validate`. Она предельно проста, так внутри требуется вызвать лишь метод `verifyUser` сервиса `authService`.

---
+++ typoteka/apps/users/src/app/auth/strategies/local.strategy.ts
@@ -0,0 +1,22 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-local';
+import { BlogUserEntity } from '../../blog-user/blog-user.entity';
+import { Injectable } from '@nestjs/common';
+import { AuthService } from '../auth.service';
+
+const USERNAME_FIELD_NAME = 'email';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private authService: AuthService
+  ) {
+    super({
+      usernameField: USERNAME_FIELD_NAME
+    });
+  }
+
+  public async validate(email: string, password: string): Promise<BlogUserEntity> {
+    return this.authService.verifyUser({ email, password });
+  }
+}
