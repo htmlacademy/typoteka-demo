44c0ff765a30173c0499800c602f506a1bcc8bfd
Igor Antonov <antonov.igor.khv@gmail.com>
2023-01-13 15:32:54 +0300

    8.26. Подключим `RefreshModule` в `AuthModule`.
    Чтобы воспользоваться недавно созданным `RefreshModule` в `AuthService`, импортируем его в `AuthModule`.

--- typoteka/apps/users/src/app/auth/auth.module.ts
+++ typoteka/apps/users/src/app/auth/auth.module.ts
@@ -12,11 +12,13 @@ import { RABBITMQ_SERVICE } from './auth.constant';
 import { getRabbitMqConfig } from '../../config/rabbitmq.config';
 import { LocalStrategy } from './strategies/local.strategy';
 import { JwtRefreshStrategy } from './strategies/jwt-refresh.strategy';
+import { RefreshTokenModule } from '../refresh-token/refresh-token.module';
 
 @Module({
   imports: [
     BlogUserModule,
     PassportModule,
+    RefreshTokenModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
