da643e45f1038ff210f6fe98128d4318020d8830
Igor Antonov <antonov.igor.khv@gmail.com>
2023-01-13 15:35:50 +0300

    8.27. Добавим проверку в `JwtRefreshStrategy`.
    Добавим дополнительную проверку в `JwtRefreshStrategy`. Подключим `RefreshTokenService` и внутри метода `validate` убедимся, что полученный токен существует в базе данных. Получается, что мы добавляем дополнительную проверку. Сначала Passport проверяет валидность полученного Refresh-токена. Затем дополнительно проверяем, что этот токен актуален. Если токен уже был активирован или отозван, то бросаем ошибку `TokenNotExistsException`. В противном случае возвращаем `payload`, он будет доступен в объекте `Request`, свойстве `user`.

--- typoteka/apps/users/src/app/auth/strategies/jwt-refresh.strategy.ts
+++ typoteka/apps/users/src/app/auth/strategies/jwt-refresh.strategy.ts
@@ -5,12 +5,15 @@ import { Inject, Injectable } from '@nestjs/common';
 import { jwtOptions } from '../../../config/jwt.config';
 import { ConfigType } from '@nestjs/config';
 import { ExtractJwt } from 'passport-jwt';
-import { TokenPayload } from '@typoteka/shared-types';
+import { RefreshTokenPayload } from '@typoteka/shared-types';
+import { RefreshTokenService } from '../../refresh-token/refresh-token.service';
+import { TokenNotExistsException } from '../exceptions/token-not-exists.exception';
 
 @Injectable()
 export class JwtRefreshStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
   constructor(
     @Inject(jwtOptions.KEY) private readonly jwtConfig: ConfigType<typeof jwtOptions>,
+    private readonly refreshTokenService: RefreshTokenService,
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -19,7 +22,11 @@ export class JwtRefreshStrategy extends PassportStrategy(Strategy, 'jwt-refresh'
     });
   }
 
-  public async validate(_req: Request, payload: TokenPayload) {
+  public async validate(_req: Request, payload: RefreshTokenPayload) {
+    if (! await this.refreshTokenService.isExists(payload.refreshTokenId)) {
+      throw new TokenNotExistsException(payload.refreshTokenId);
+    }
+
     return payload;
   }
 }
