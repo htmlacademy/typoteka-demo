c1c3d788491f02f7bd03b86567913ff1f44234ab
Igor Antonov <antonov.igor.khv@gmail.com>
2023-03-22 16:44:39 +0300

    2.8. Добавит модули `AuthenticationModule` и `BlogUserModule`.
    Быстро накидать структуру приложения поможет команда `generator` и
    соответствующий пресет. Например, для создания новых модулей `auth`
    и `blog-user` выполним две команды:
    
    ```
    nx generate @nrwl/nest:module app/blog-user --project=users
    nx generate @nrwl/nest:module app/authentication --project=users
    ```
    
    Обратите внимание, будут созданы одноимённые директории `authentication` и
    `blog-user`, а в них заготовки для модулей.
    
    В корневой модуль приложения будут автоматически внесены изменения:
    будут подключены модули `AuthenticationModule` и `BlogUserModule`.

--- project/apps/users/src/app/app.module.ts
+++ project/apps/users/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AuthenticationModule } from './authentication/authentication.module';
+import { BlogUserModule } from './blog-user/blog-user.module';
 
 @Module({
-  imports: [],
+  imports: [AuthenticationModule, BlogUserModule],
   controllers: [],
   providers: [],
 })
