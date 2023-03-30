7165ed071eb09afb5a4a80473acc09a136650f31
Igor Antonov <antonov.igor.khv@gmail.com>
2023-03-30 16:07:50 +0300

    3.7. Подключит библиотеку `ConfigUsersModule` в `users/AppModule`.
    Импортируем модуль из созданной библиотеки `ConfigUsersModule` в
    корневой модуль приложения `users`. Для этого добавим его в `imports`. Фактически
    на этом шаге начнёт работать валидация данных, которые мы передаём через
    переменные окружения.
    
    Попробуйте удалить какую-нибудь переменную из конфига и запустите приложение. Будет
    брошено исключение и приложение прекратит запуск.

--- project/apps/users/src/app/app.module.ts
+++ project/apps/users/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { BlogUserModule } from './blog-user/blog-user.module';
+import { ConfigUsersModule } from '@project/config/config-users';
 
 @Module({
-  imports: [AuthenticationModule, BlogUserModule],
+  imports: [AuthenticationModule, BlogUserModule, ConfigUsersModule],
   controllers: [],
   providers: [],
 })
