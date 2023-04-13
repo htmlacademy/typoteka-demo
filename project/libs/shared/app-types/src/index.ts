190ebf096ed0651d0939cda14edfea1e27cca8f3
Igor Antonov <antonov.igor.khv@gmail.com>
2023-04-13 17:31:37 +0300

    5.28. Добавит интерфейс `TokenPayload`.
    В `AccessToken` мы будем передать информацию о пользователе: id, email и так далее.
    Опишем для этого отдельный интерфейс `TokenPayload`.

--- project/libs/shared/app-types/src/index.ts
+++ project/libs/shared/app-types/src/index.ts
@@ -4,3 +4,4 @@ export * from './lib/category.interface';
 export * from './lib/comment.interface';
 export * from './lib/post.interface';
 export * from './lib/file.interface';
+export * from './lib/token-payload.interface';
