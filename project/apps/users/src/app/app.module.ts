9e9b159465da55a8d32491cd8cbe96c8f733a878
Igor Antonov <antonov.igor.khv@gmail.com>
2023-04-19 16:47:56 +0300

    6.12. Добавит модуль `Notify` в сервис `users`.
    Чтобы полноценно протестировать добавление подписчиков, добавим в сервис
    `users` возможность взаимодействовать с RabbitMQ. Для этого обновим
    конфигурацию приложения и добавим новый модуль `Notify`.
    
    Отправку сообщений в RabbitMQ сделаем в сервисе `Notify`.

--- project/apps/users/src/app/app.module.ts
+++ project/apps/users/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { AuthenticationModule } from './authentication/authentication.module';
 import { BlogUserModule } from './blog-user/blog-user.module';
 import { ConfigUsersModule, getMongooseOptions } from '@project/config/config-users';
 import { MongooseModule } from '@nestjs/mongoose';
+import { NotifyModule } from './notify/notify.module';
 
 @Module({
   imports: [
     AuthenticationModule,
     BlogUserModule,
     ConfigUsersModule,
+    NotifyModule,
     MongooseModule.forRootAsync(
       getMongooseOptions()
   )],
