fb6cfaac61946f16dc7cf63b0626be47bafa8310
Igor Antonov <antonov.igor.khv@gmail.com>
2023-08-01 17:10:33 +0300

    4.26. Подключит `PrismaModule` и `BlogCategoryModule` в `AppModule`.

--- project/apps/blog/src/app/app.module.ts
+++ project/apps/blog/src/app/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { BlogCategoryModule } from './blog-category/blog-category.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, BlogCategoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
