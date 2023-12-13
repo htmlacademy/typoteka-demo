a840953e1fcf40ca03be993f028eee979fb5f450
Igor Antonov <antonov.igor.khv@gmail.com>
2023-12-13 18:20:01 +0300

    3.7. Подключит `ConfigAccountModule` в account.
    Импортируем модуль из созданной библиотеки `ConfigAccountModule` в корневой модуль приложения `account`. Для этого добавим его в `imports`. Фактически
    на этом шаге начнёт работать валидация данных, которые мы передаём через переменные окружения.
    
    Попробуйте удалить какую-нибудь переменную из конфига и запустите приложение. Будет брошено исключение и приложение прекратит запуск.

--- project/apps/account/src/app/app.module.ts
+++ project/apps/account/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { BlogUserModule } from './blog-user/blog-user.module';
+import { ConfigAccountModule } from '@project/shared/config/account';
 
 @Module({
-  imports: [AuthenticationModule, BlogUserModule ],
+  imports: [AuthenticationModule, BlogUserModule, ConfigAccountModule],
   controllers: [],
   providers: [],
 })
