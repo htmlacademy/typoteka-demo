17b2aafd3705ed24fd069ee9fb563e898dc87abf
Igor Antonov <antonov.igor.khv@gmail.com>
2023-12-13 19:08:39 +0300

    3.16. Добавит функцию `getMongooseOptions`.
    Теперь обсудим работу с обёрткой для Mongoose. Речь о
    `@nestjs/mongoose`. Обёртка предоставляет отдельный модуль, упрощающий интеграцию с Mongo. Для этого он предоставляет отдельный объект `MongooseModule`. Общий принцип работы с этим объектом точно такой же как с `ConfigModule`: импортируем из пакета, передаём в импорты (`imports`) главного модуля приложения.
    
    Дальше мы можем воспользоваться методом `forRoot` и передать настройки для подключения к СУБД. Есть только одно «но». Мы заранее не знаем про настройки. Мы их получаем через `ConfigModule`.
    
    Поэтому, простой вариант нам не подойдёт. Нужно дождаться, когда
    загрузятся другие модули, а лишь потом приступать к загрузке модуля
    для работы с Mongoose.
    
    В связи с этим мы воспользуемся методом `forRootAsync`. Методу нужно
    передать настройки типа `MongooseModuleAsyncOptions`. Для этого мы
    подготовим отдельную функцию `getMongooseOptions`. Он вернёт фабрику, в которую мы внедрим сервис управления конфигурацией (`configService`).
    
    Чтобы внедрение зависимостей сработало, необходимо не забыть указать
    `ConfigService` в качестве внедряемого сервиса в свойстве `inject`.
    Внутри `getMongooseOptions()` воспользуемся ранее подготовленной функцией `getMongoConnectionString`.

--- project/shared/config/account/src/index.ts
+++ project/shared/config/account/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/config-account.module';
 export { default as applicationConfig } from './lib/app.config';
 export { default as dbConfig } from './lib/mongo.config';
+export * from './lib/mongodb/get-mongoose-options';
