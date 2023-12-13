464b05ecbd26acb1fc0acfea1364d718bb40b17b
Igor Antonov <antonov.igor.khv@gmail.com>
2023-12-13 19:15:33 +0300

    3.17. Подключит `MongooseModule` в корневой модуль сервиса `account`.
    Последним шагом на пути к установке соединения с MongoDB станет
    добавления `MongooseModule`. Добавляем в секцию `imports` корневого
    модуля приложения и в метод `forRootAsync` передаём результат выполнения `getMongooseOptions`.
    
    После этого шага можно запустить приложение и убедиться, что ошибок не возникает. Установка соединения с MongoDB проходит.

--- project/apps/account/src/app/app.module.ts
+++ project/apps/account/src/app/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AuthenticationModule } from './authentication/authentication.module';
 import { BlogUserModule } from './blog-user/blog-user.module';
-import { ConfigAccountModule } from '@project/shared/config/account';
+import { ConfigAccountModule, getMongooseOptions } from '@project/shared/config/account';
 
 @Module({
-  imports: [AuthenticationModule, BlogUserModule, ConfigAccountModule],
+  imports: [
+    AuthenticationModule,
+    BlogUserModule,
+    ConfigAccountModule,
+    MongooseModule.forRootAsync(
+      getMongooseOptions()
+    )
+  ],
   controllers: [],
   providers: [],
 })
