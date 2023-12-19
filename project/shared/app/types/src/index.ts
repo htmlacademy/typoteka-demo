ebd5bac52c8b3ea61335f48d023420743b7b1c17
Igor Antonov <antonov.igor.khv@gmail.com>
2023-12-19 17:44:23 +0300

    4.16. Добавит интерфейсы для сущностей блога.
    Создадим интерфейсы `Category`, `Comment`, `Post`. Все интерфейсы создаём в общей библиотеке «app/types».

--- project/shared/app/types/src/index.ts
+++ project/shared/app/types/src/index.ts
@@ -1,3 +1,6 @@
 export * from './lib/user-role.enum';
 export * from './lib/user.interface';
 export * from './lib/auth-user.interface';
+export * from './lib/category.interface';
+export * from './lib/comment.interface';
+export * from './lib/post.interface';
