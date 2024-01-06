076e6597204dd01e244d28a9b092e5f7f1a21eb6
Igor Antonov <antonov.igor.khv@gmail.com>
2024-01-06 22:34:31 +0300

    5.32. Добавит модуль `BlogComment`.
    По аналогии создадим модуль для управления комментариями — `BlogComment`. Как и в прошлый раз, нам потребуется `BlogCommentEntity`, `BlogCommentRepository` и так далее.
    
    Для проверки выполнения запросов подготовим `blog-comment.http`.
    
    Из нового: при использовании декоратора `Controller` укажем путь для ресурса с параметром.

--- project/apps/blog/src/app/app.module.ts
+++ project/apps/blog/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BlogCategoryModule } from './blog-category/blog-category.module';
 import { BlogPostModule } from './blog-post/blog-post.module';
+import { BlogCommentModule } from './blog-comment/blog-comment.module';
 
 @Module({
-  imports: [BlogCategoryModule, BlogPostModule],
+  imports: [BlogCategoryModule, BlogPostModule, BlogCommentModule],
   controllers: [AppController],
   providers: [AppService],
 })
