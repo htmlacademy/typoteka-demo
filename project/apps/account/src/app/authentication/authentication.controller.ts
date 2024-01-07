a002721269405f0a6d85f69d848fb72d5af05a6d
Igor Antonov <antonov.igor.khv@gmail.com>
2024-01-07 10:01:57 +0300

    5.35. Подключит `MongoIdValidationPipe` в `account`
    Воспользуемся созданным пайпом и подключим его к обработчику `show` контроллера `AuthenticationController`. Этот служебный обработчик возвращает клиенту полную информацию о пользователе. Для запроса клиента передаёт идентификатор пользователя — MongoID. Его как раз и нужно валидировать.
    
    Подключим `MongoIdValidationPipe` в декоратор `Param` обработчика `show`. После этого запустите приложение и попробуйте передать невалидный идентификатор ObjectID. В примере запросами как раз такой и передаётся. Вместо остановки приложения по ошибке, будет возвращён код состояния 400 от сервера.
    
    Позже мы воспользуемся этим пайпом и в других сервисах.

--- project/apps/account/src/app/authentication/authentication.controller.ts
+++ project/apps/account/src/app/authentication/authentication.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, HttpStatus, Param, Post, ParseIntPipe } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Param, Post } from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
 import { AuthenticationService } from './authentication.service';
@@ -7,6 +7,7 @@ import { fillDto } from '@project/shared/helpers';
 import { UserRdo } from './rdo/user.rdo';
 import { LoginUserDto } from './dto/login-user.dto';
 import { LoggedUserRdo } from './rdo/logged-user.rdo';
+import { MongoIdValidationPipe } from '@project/shared/core';
 
 @ApiTags('authentication')
 @Controller('auth')
@@ -46,7 +47,7 @@ export class AuthenticationController {
     description: 'User found'
   })
   @Get(':id')
-  public async show(@Param('id') id: string) {
+  public async show(@Param('id', MongoIdValidationPipe) id: string) {
     const existUser = await this.authService.getUser(id);
     return fillDto(UserRdo, existUser.toPOJO());
   }
