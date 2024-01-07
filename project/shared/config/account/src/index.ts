1794bbb19cc7342476fb851844b887e51ca3073a
Igor Antonov <antonov.igor.khv@gmail.com>
2024-01-07 12:47:52 +0300

    5.38. Добавит настройки для JWT.
    Для формирования JWT потребуются минимум два значения:
    
    * Секрет. Он будет использоваться для формирования подписи токена;
    * Время жизни токена. Оно всегда ограничено и нужно определиться сколько времени будет валиден токен.
    
    Эти значения могут меняться, поэтому их нужно передавать снаружи. Создадим для сервиса `accounts` конфигурацию для JWT. Определим две настройки, которые будем задавать через переменные окружения:
    `JWT_ACCESS_TOKEN_SECRET` и `JWT_ACCESS_TOKEN_EXPIRES_IN`.
    
    Опишем для них схему валидации.

--- project/shared/config/account/src/index.ts
+++ project/shared/config/account/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/config-account.module';
 export { default as applicationConfig } from './lib/app.config';
 export { default as dbConfig } from './lib/mongo.config';
 export * from './lib/mongodb/get-mongoose-options';
+export { default as jwtConfig } from './lib/jwt.config';
